754dd9cc3ba72b38a472f893e956fb05c80ea88c
Oliver Wigren <oliver.wigren@hotmail.com>
2023-08-09 14:22:10 +0200

    primary gameplay
    -created scripts for oven and betteroven
    -created gameobjects for the ovens

---
+++ Assets/Oven.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oven : MonoBehaviour
+{
+    public bool cooking { get; private set; }
+
+    private void OnMouseDown()
+    {
+        
+    }
+}
