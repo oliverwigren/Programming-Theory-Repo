a3eb380e391bd2ffceb04a3c1e0bee5630711886
Oliver Wigren <oliver.wigren@hotmail.com>
2023-08-10 19:29:08 +0200

    encapsulation
    -encapsulation

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,10 +5,17 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public int totalScore { set
+    private int fallbackTotalScore;
+    public int totalScore {
+        get
         {
-            if (value < 0) { Debug.LogError("score cannot be a negative number");}
-        } 
+            return totalScore;
+        }
+        set 
+        {
+            if (value < 0) { Debug.LogError("score cannot be a negative number"); }
+            else { fallbackTotalScore = value; }
+        }
     } 
     private Text titleText;
     private Text scoreText;
@@ -23,14 +30,22 @@ public class GameManager : MonoBehaviour
     {
         titleText = GameObject.Find("Title").GetComponent<Text>();
         SaveManager.Instance.LoadName();
+        if (SaveManager.Instance.name == null)
+        {
+            Debug.LogWarning("No Bakery-name was found, default name will be used");
+            titleText.text = "Bakery";
+        }
+        else
+        {
         titleText.text = SaveManager.Instance.name;
+        }
         scoreText = GameObject.Find("Score").GetComponent<Text>();
         scoreText.text = "SCORE: 0";
     }
 
     public void UpdateScore(int addition)
     {
-        totalScore += addition;
-        scoreText.text = "SCORE: " + totalScore;
+        totalScore -= addition;
+        scoreText.text = "SCORE: " + fallbackTotalScore;
     }
 }
